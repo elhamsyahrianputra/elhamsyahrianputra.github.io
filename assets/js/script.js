68b22b9415a58369cd6303b6e4d8ce8c7a7939b6
elhamsyahrianputra <elhamsyahrianputra220402@gmail.com>
2021-11-07 04:24:25 +0800

    Add Mesaage in Whatsapp Direct, add hover at catalog

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,7 +67,7 @@ kirim.addEventListener('click', function() {
     sesi = document.getElementById('sesi').value;
     catatan = document.getElementById('catatan').value
 
-    window.location = "https://web.whatsapp.com/send?phone=6281346651603&text=-------*" + modalTitle + "*-------%0A" + "%0A*Nama%20Lengkap*%20%3A%20" + nama + "%0A*Alamat*%20%3A%20" + alamat + "%0A*Jenis%20Sampah*%20%3A%20"+ arr + "%0A*Sesi%20Pengambilan*%20%3A%20" + sesi + "%0A*Catatan*%20%3A%20" + catatan;
+    window.location = "https://wa.me/send?phone=6281346651603&text=-------*" + modalTitle + "*-------%0A" + "%0A*Nama%20Lengkap*%20%3A%20" + nama + "%0A*Alamat*%20%3A%20" + alamat + "%0A*Jenis%20Sampah*%20%3A%20"+ arr + "%0A*Sesi%20Pengambilan*%20%3A%20" + sesi + "%0A*Catatan*%20%3A%20" + catatan + "%0A%0A*Mohon sertakan juga gambar dari barang yang ingin anda jual";
 
 });
 
